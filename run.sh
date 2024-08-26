2b09300aaa3374c588f7de311a5c22204415512c
Manjunath R <ManjunathR@ibm.com>
2024-08-26 19:13:18 +0530

    added the ccomments in the run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------
+# This script help to run in the editable mode where you can develop and test at the run time
+# -------------------------------------------------------------------------------------------
 pip uninstall mcmd
 rm -rf build dist *egg* __pycache__
 python setup.py sdist bdist_wheel
